f8ec0cef4c9c064b2cd6a65b4c86bf0dbacd7e28
Manjeet2001 <manjeetydv16@gmail.com>
2024-08-13 13:29:47 +0530

    Start of Project

---
+++ src/main/java/com/example/EmployeeDataManager/EmployeeDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeDataManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDataManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmployeeDataManager/EmployeeDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeDataManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
